962b5d79e3046017341192cbf337c16fda98fb3b
zhid0399123 <zhid0399123@gmail.com>
2024-10-12 17:08:34 +0300

    deleted

--- changelog.md
+++
@@ -1,7 +0,0 @@
-## [v0.3.0]() April 17, 2024
-
-Added support to parse array data types.
-
-## [v0.2.0]() April 11, 2024
-
-to-object: Initial release.
--- src/lib/strip-export.js
+++
@@ -1,9 +0,0 @@
-export default function stripExports(line) {
-  function matchExport() {
-    return line.match(/^export\s+\w+.*=\s*|^export\s+.*/);
-  }
-  const match = matchExport()
-  if (match && match.length)
-    line = line.replace(line.substring(line.indexOf(match[0]), match[0].length), "")
-  return line
-}
